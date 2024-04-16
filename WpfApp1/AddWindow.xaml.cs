8a667d4eb1e386b0e05e56e7d2c7d19e0b4c08f6
22IB23_PITOR <22IB23_PITOR@U1-11>
2024-04-16 09:21:25 +0200

    Přidejte soubory projektu.

---
+++ WpfApp1/AddWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interakční logika pro AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string GetRecord()
+        {
+            string jmeno = Jmeno.Text;
+            string prijmeni = Prijmeni.Text;
+            string id = ID1.Text;
+            string date = datePicker.SelectedDate?.ToShortDateString();
+            return $"Jmeno: {jmeno}/  Příjmení: {prijmeni}/ ID : {id} - DATUM: {date}";
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
+
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddRecord_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow addRecordWindow = new AddWindow();
+            if (addRecordWindow.ShowDialog() == true)
+            {
+                listBox.Items.Add(addRecordWindow.GetRecord());
+            }
+        }
+
+        private void DeleteRecord_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                listBox.Items.Remove(listBox.SelectedItem);
+            }
+        }
+        private void EditZamestnance_Click(object sender, RoutedEventArgs e) 
+        {
+          EditEmployee editEmployee = new EditEmployee();
+
+            if (editEmployee.ShowDialog() == true)
+            {
+                listBox.Items.Refresh();
+            }
+        }
+
+        
+    }
+}
